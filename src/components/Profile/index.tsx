c70b8dbe95fbbd8f0c3966ffba0e0faf92d0e4f8
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-03-04 01:55:31 -0300

    Add files via upload

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -7,10 +7,10 @@ const Profile = () => {
   const { level } = useContext(ChallengesContext);
   return (
     <Container>
-      <img src="https://github.com/martins-rafael.png" alt="Rafael Martins" />
+      <img src="https://github.com/sozinhol.png" alt="Jonatas Souza Soares" />
 
       <div>
-        <strong>Rafael Martins</strong>
+        <strong>Jonatas Souza Soares</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level {level}
